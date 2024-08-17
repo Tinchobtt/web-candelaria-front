5b3c04b28673464cccf58b1e54c36c9151882b4b
Martin <martin.benedetto07@gmail.com>
2024-08-17 13:55:49 -0300

    Creado de componente header - Agregado de logo - implementacion de scss

--- src/components/common/redes/Redes.jsx
+++ src/components/common/redes/Redes.jsx
@@ -1,4 +1,4 @@
-import './redes.css'
+import './redes.scss'
 import { FaPhoneAlt } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
