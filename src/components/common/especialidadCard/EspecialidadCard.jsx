8fe93dd8c71af3e4da2f23895231730bdd251f4b
Martin <martin.benedetto07@gmail.com>
2024-08-18 16:33:51 -0300

    Creado de seccion Especialidades y EspecialidadCard

---
+++ src/components/common/especialidadCard/EspecialidadCard.jsx
@@ -0,0 +1,20 @@
+import './especialidadCard.scss'
+import vacioImg from '../../../assets/imgs/1.png'
+
+const EspecialidadCard = ({left}) => {
+    return (
+        <div className='especialidadCard'>
+            <div className="especialidadCard-img">
+                <div className='circle'>
+                    <img src={vacioImg} alt=""/>
+                </div>
+            </div>
+            <div className={left ? "especialidadCard-content contentLeft" : "especialidadCard-content"}>
+                <h3>Vacío al asador</h3>
+                <p>Carne elaborada a las leñas con cocción lenta acompañada con ensalada de la huerta</p>
+            </div>
+        </div>
+    )
+}
+
+export default EspecialidadCard
