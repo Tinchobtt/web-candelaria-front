e506c255778904a550067ab10a334a1ba1fe249d
Martin <martin.benedetto07@gmail.com>
2024-08-23 00:30:35 -0300

    Modal Structure

--- src/components/common/genericModal/GenericModal.jsx
+++ src/components/common/genericModal/GenericModal.jsx
@@ -1,6 +1,7 @@
 import ModalReserva from "../modalReserva/ModalReserva.jsx";
 import {useModal} from "../../../context/ModalContext.jsx";
 import {useEffect, useRef} from "react";
+import './genericModal.scss'
 
 const ModalContent = {
     reservaMesa: <ModalReserva name={'Mesa'} />,
@@ -28,40 +29,13 @@ const GenericModal = () => {
     if(!modalType) return null
 
     return (
-        <div style={styles.overlay}>
-            <div ref={modalRef} style={styles.modal}>
-                <button onClick={closeModal} style={styles.closeButton}>X</button>
+        <div className="overlay">
+            <div ref={modalRef} className="modal">
+                <button onClick={closeModal} className="closeButton">X</button>
                 { ModalContent[modalType] }
             </div>
         </div>
     );
 };
-const styles = {
-    overlay: {
-        position: 'fixed',
-        top: 0,
-        left: 0,
-        width: '100%',
-        height: '100%',
-        backgroundColor: 'rgba(0, 0, 0, 0.5)',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        zIndex: '100000'
-    },
-    modal: {
-        padding: '20px',
-        borderRadius: '5px',
-        position: 'relative',
-    },
-    closeButton: {
-        position: 'absolute',
-        top: '10px',
-        right: '10px',
-        border: 'none',
-        background: 'transparent',
-        cursor: 'pointer',
-        fontSize: '20px',
-    },
-};
+
 export default GenericModal;
