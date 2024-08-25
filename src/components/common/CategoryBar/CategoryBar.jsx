6fe35580fb773754f63e6653609f5bc1e17c1626
Martin <martin.benedetto07@gmail.com>
2024-08-25 18:08:08 -0300

    Update CategoryBar.jsx

--- src/components/common/CategoryBar/CategoryBar.jsx
+++ src/components/common/CategoryBar/CategoryBar.jsx
@@ -1,14 +1,33 @@
-const CategoryBar = ( {categories, filterProductsByCategory} ) =>{
-    return(
-        <>
-            <button onClick={() => filterProductsByCategory()}>Todos</button>
-            {
+import './categoryBar.scss'
+import {useState} from "react";
 
-                categories.map(category =>
-                    <button key={category} onClick={() => filterProductsByCategory(category)}>{category}</button>
-                )
-            }
-        </>
+const CategoryBar = ( {categories, filterProductsByCategory} ) =>{
+    const [actuaCategory, setActuaCategory] = useState('todos')
+
+    const selectCategory = (category) =>{
+        filterProductsByCategory(category)
+        setActuaCategory(category)
+    }
+
+    return(
+        <div className="category-bar">
+            <div className="category-slider">
+                <button
+                    className={actuaCategory == 'todos' ? 'category-btn active' : 'category-btn'} o
+                    nClick={() => selectCategory('todos')}
+                >Todos</button>
+                {
+                    categories &&
+                    categories.map(category =>
+                        <button
+                            className={actuaCategory == category ? 'category-btn active' : 'category-btn'}
+                            key={category}
+                            onClick={() => selectCategory(category)}
+                        >{category}</button>
+                    )
+                }
+            </div>
+        </div>
     )
 }
 
