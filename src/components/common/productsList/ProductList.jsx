565a33fa89e6d4cc32ef05d929580a45b457bad0
Tinchobtt <martin.benedetto07@gmail.com>
2024-08-26 00:01:43 -0300

    Page eventos - definir cards

--- src/components/common/productsList/ProductList.jsx
+++ src/components/common/productsList/ProductList.jsx
@@ -9,13 +9,11 @@ const ProductList = ({products}) =>{
                 products &&
                 products.map(product => (
                     <ProductCard
-                        key={product.id}
+                        key={product.title}
                         title={product.title}
                         description={product.description.slice(0, 100)}
                         price={product.price}
-                        // actualPrice={product.actualPrice}
-                        actualPrice={product.price}
-                        // img={product.img}
+                        actualPrice={product.actualPrice}
                         img={product.image}
                     />
                 ))
--- src/components/pages/admin/Admin.jsx
+++ src/components/pages/admin/Admin.jsx
@@ -1,8 +1,19 @@
-const Admin = () => {
-    return (
-        <>
+import {useProductsCategories} from "../../../context/ProductsCategoriesContext.jsx";
+import {useEffect} from "react";
 
-        </>
+const Admin = () => {
+    const { products, categories, isLoading, active, setActive, filterProductsByCategory, fetchData} = useProductsCategories()
+
+    useEffect(() => {
+        if ((!products || products.length === 0) && !isLoading) {
+            fetchData(false);
+        }
+    }, [products, active]);
+
+    return (
+        <main>
+
+        </main>
     )
 }
 
