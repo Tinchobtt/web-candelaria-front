97da28a452b9d98536cc1d69ca7202832377a707
Martin <martin.benedetto07@gmail.com>
2024-09-19 00:10:30 -0300

    SemiMocks

--- src/components/pages/admin/Admin.jsx
+++ src/components/pages/admin/Admin.jsx
@@ -5,14 +5,14 @@ import Header from "../../layout/header/Header.jsx";
 import { Outlet } from 'react-router-dom';
 
 const Admin = () => {
-    const { products, isLoading, fetchData} = useProductsCategories()
+    const { products, categories, isLoading, fetchData} = useProductsCategories()
 
     useEffect(() => {
         if ((!products || products.length === 0) && !isLoading) {
             fetchData(false);
         }
-    }, [products]);
-
+    }, [products, categories]);
+    
     return (
         <div className="admin-container">
             <Header />
