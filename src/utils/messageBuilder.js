dce85a25e1c5effcfb7e7ec0accb732a82a98d72
Martin <martin.benedetto07@gmail.com>
2024-10-11 19:21:56 -0300

    Deteccion de dispositivo

--- src/utils/messageBuilder.js
+++ src/utils/messageBuilder.js
@@ -2,6 +2,7 @@ export const messageBuilder = (tipoMensaje, values, tipoPedido, metodoPago, prod
     const phoneNumber = 541125372314;
     let message = '';
 
+    // Construcción del mensaje basado en el tipo de mensaje
     if (tipoMensaje === 'pedido') {
         message = `Hola, me llamo ${values.name}. Quiero hacer un pedido para el ${values.date} a las ${values.time} hs.`;
         message += `\nTipo de pedido: ${tipoPedido}\nMétodo de pago: ${metodoPago}`;
@@ -24,8 +25,19 @@ export const messageBuilder = (tipoMensaje, values, tipoPedido, metodoPago, prod
         message += `\nMensaje adicional: ${values.message}`;
     }
 
+    // Detectar si el dispositivo es móvil
+    const isMobileDevice = () => {
+        const userAgent = navigator.userAgent || window.opera;
+        const mobileDevices = /android|iphone|ipad|ipod|blackberry|iemobile|opera mini|windows phone|webos|silk|avantgo|nokia|palm|kindle|symbian|mobile|phone/i;
+        return mobileDevices.test(userAgent.toLowerCase());
+    };
+
     const encodedMessage = encodeURIComponent(message);
-    const whatsappUrl = `https://web.whatsapp.com/send?phone=${phoneNumber}&text=${encodedMessage}`;
+
+    // Usar URL diferente según el dispositivo
+    const whatsappUrl = isMobileDevice() 
+        ? `https://wa.me/${phoneNumber}?text=${encodedMessage}` // Para móviles
+        : `https://web.whatsapp.com/send?phone=${phoneNumber}&text=${encodedMessage}`; // Para escritorio
 
     return whatsappUrl;
-}
+};
