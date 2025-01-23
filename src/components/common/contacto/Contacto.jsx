c52b36c18dcdfdb6c6f4b57a270dd70d72a1054b
Martin <martin.benedetto07@gmail.com>
2025-01-23 18:24:28 -0300

    * Fix horarios

--- src/components/common/contacto/Contacto.jsx
+++ src/components/common/contacto/Contacto.jsx
@@ -23,31 +23,31 @@ const Contacto = () => {
                         <div className="schedule">
                             <div className="day">
                                 <span>Domingo</span>
-                                <span className="time">10:00 - 23.00 hs</span>
+                                <span className="time">10:00 - 23:00 hs</span>
                             </div>
                             <div className="day">
                                 <span>Lunes</span>
-                                <span className="time">10:00 - 23.00 hs</span>
+                                <span className="time">10:00 - 23:00 hs</span>
                             </div>
                             <div className="day">
                                 <span>Martes</span>
-                                <span className="time">10:00 - 23.00 hs</span>
+                                <span className="time">10:00 - 23:00 hs</span>
                             </div>
                             <div className="day">
                                 <span>Miércoles</span>
-                                <span className="time">10:00 - 23.00 hs</span>
+                                <span className="time">10:00 - 23:00 hs</span>
                             </div>
                             <div className="day">
                                 <span>Jueves</span>
-                                <span className="time">10:00 - 23.00 hs</span>
+                                <span className="time">10:00 - 23:00 hs</span>
                             </div>
                             <div className="day">
                                 <span>Viernes</span>
-                                <span className="time">10:00 - 23.30 hs</span>
+                                <span className="time">10:00 - 23:30 hs</span>
                             </div>
                             <div className="day">
                                 <span>Sábado</span>
-                                <span className="time">10:00 - 23.30 hs</span>
+                                <span className="time">10:00 - 23:30 hs</span>
                             </div>
                         </div>
                     </div>
